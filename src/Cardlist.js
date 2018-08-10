415eef6b50f27a3549050dab62598655a2d568a3
vizz1234 <34003673+vizz1234@users.noreply.github.com>
2018-08-10 18:34:51 +0530

    Add files via upload

---
+++ src/Cardlist.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Card';
+const Cardlist=({ robots })=>
+{	
+	return (
+		<div>
+		{
+			robots.map((user,i) => {
+				return(
+					<Card 
+					key={i}
+					id={robots[i].id}
+					name={robots[i].name}
+					email={robots[i].email}
+					username={robots[i].username}
+					/>
+					);
+			})
+		}
+		</div>
+		);
+	}
+export default Cardlist;

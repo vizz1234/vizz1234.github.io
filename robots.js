aab72fe0b8de767a3eba39f16e9ff4cc479b38eb
vizz1234 <34003673+vizz1234@users.noreply.github.com>
2018-08-10 18:25:11 +0530

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,72 @@
+export const robots = [
+ 
+ {
+    id: 1,
+    name: 'Vishwanath',
+    username: 'Vizz',
+    email: 'Sincere@april.biz'
+  },
+  
+{
+    id: 2,
+    name: 'Shreyes',
+    username: 'Shr',
+    email: 'Shanna@melissa.tv'
+  },
+ 
+ {
+    id: 3,
+    name: 'AppaSaheb',
+    username: 'Rushi',
+    email: 'Nathan@yesenia.net'
+  },
+  
+{
+    id: 4,
+    name: 'Vivek',
+    username: 'Buss',
+    email: 'Julianne.OConner@kory.org'
+  },
+  
+{
+    id: 5,
+    name: 'Ramesh',
+    username: 'Ram',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  
+{
+    id: 6,
+    name: 'Varun',
+    username: 'Varun',
+    email: 'Karley_Dach@jasper.info'
+  },
+  
+{
+    id: 7,
+    name: 'Abhilash',
+    username: 'Tharki',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  
+{
+    id: 8,
+    name: 'Gowtham',
+    username: 'Gauti',
+    email: 'Sherwood@rosamond.me'
+  },
+  
+{
+    id: 9,
+    name: 'Chinmay',
+    username: 'Chinnu',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  
+{
+    id: 10,
+    name: 'Rohan',
+    username: 'Kalal',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
